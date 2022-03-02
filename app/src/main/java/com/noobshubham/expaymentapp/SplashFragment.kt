4ed839675439b6c844610165e214c96dff519773
noobshubham <noobshubham65@gmail.com>
2022-03-02 21:43:21 +0530

    Minor Fixes.

--- app/src/main/java/com/noobshubham/expaymentapp/SplashFragment.kt
+++ app/src/main/java/com/noobshubham/expaymentapp/SplashFragment.kt
@@ -1,12 +1,17 @@
 package com.noobshubham.expaymentapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.findNavController
 import com.noobshubham.expaymentapp.databinding.FragmentSplashBinding
+
+
+
 
 class SplashFragment : Fragment() {
 
