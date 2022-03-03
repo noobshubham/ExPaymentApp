99611482419e4ee8f4e02d5344f0bfc3294f532c
noobshubham <noobshubham65@gmail.com>
2022-03-03 10:53:15 +0530

    Added Code to hide action bar for SplashFragment.kt

--- app/src/main/java/com/noobshubham/expaymentapp/SplashFragment.kt
+++ app/src/main/java/com/noobshubham/expaymentapp/SplashFragment.kt
@@ -1,7 +1,6 @@
 package com.noobshubham.expaymentapp
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -10,13 +9,22 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.findNavController
 import com.noobshubham.expaymentapp.databinding.FragmentSplashBinding
 
-
-
-
 class SplashFragment : Fragment() {
 
     private var _binding: FragmentSplashBinding? = null
     private val binding get() = _binding!!
+
+    // this code is to hide the action bar
+    override fun onResume() {
+        super.onResume()
+        (activity as AppCompatActivity?)!!.supportActionBar!!.hide()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        (activity as AppCompatActivity?)!!.supportActionBar!!.show()
+    }
+    // this is end of the hide action bar code
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
