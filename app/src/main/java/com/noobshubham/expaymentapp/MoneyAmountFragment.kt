41f9838f658c2f25b71a3a4b1165aaa5d1b16a1a
noobshubham <noobshubham65@gmail.com>
2022-03-03 12:42:02 +0530

    Minor Fix for MoneyAmountFragment.kt

--- app/src/main/java/com/noobshubham/expaymentapp/MoneyAmountFragment.kt
+++ app/src/main/java/com/noobshubham/expaymentapp/MoneyAmountFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.trace
 import androidx.navigation.fragment.findNavController
 import com.noobshubham.expaymentapp.databinding.FragmentMoneyAmountBinding
 
@@ -31,11 +32,19 @@ class MoneyAmountFragment : Fragment() {
 
         binding.buttonConfirm.setOnClickListener {
             // this will crash if amount is blank
-            val amount = binding.amountOf.text.toString().toFloat()
+            val amount = binding.amountOf.text.toString().toFloatOrNull()
             Log.d(tag,"dunno why its crashing, fix this shit!")
-            val dir = MoneyAmountFragmentDirections.actionMoneyAmountFragmentToStatusFragment(args.name, amount)
-            findNavController().navigate(dir)
+            if (amount == null || amount == 0.00F) {
+                displayAmount(args.name, 0.00F)
+                return@setOnClickListener
+            }
+            displayAmount(args.name, amount)
         }
+    }
+
+    private fun displayAmount(name: String, d: Float) {
+        val dir = MoneyAmountFragmentDirections.actionMoneyAmountFragmentToStatusFragment(name, d)
+        findNavController().navigate(dir)
     }
 
     override fun onDestroyView() {
