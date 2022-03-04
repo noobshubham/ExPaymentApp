4dad03644446a9b7abd85972ffe302522b665153
noobshubham <noobshubham65@gmail.com>
2022-03-04 10:10:54 +0530

    Added GIF to StatusFragment.kt

--- app/src/main/java/com/noobshubham/expaymentapp/StatusFragment.kt
+++ app/src/main/java/com/noobshubham/expaymentapp/StatusFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
 import com.noobshubham.expaymentapp.databinding.FragmentStatusBinding
 
 class StatusFragment : Fragment() {
@@ -25,6 +26,8 @@ class StatusFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         val args = StatusFragmentArgs.fromBundle(requireArguments())
+        // loading the gif from local assesst folder https://handyopinion.com/load-gif-in-imageview-android-java-kotlin/
+        Glide.with(this.activity).load("file:///android_asset/checkmarksuccess.gif").into(binding.successful)
         binding.success.text = getString(R.string.status_msg, args.amount, args.name)
     }
 
